62ababcdc3c421a32c7c5ba1baf86e786c4a8355
thanasak036 <joice036@hotmail.com>
2015-10-16 11:15:20 +0700

    Video

--- app/src/main/java/epson/a/thanasak/epsontraffic/MainActivity.java
+++ app/src/main/java/epson/a/thanasak/epsontraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package epson.a.thanasak.epsontraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -11,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView trafficListView;
     private Button aboutMEButton;
 
-// จาน
+    // จาน
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +24,30 @@ public class MainActivity extends AppCompatActivity {
 
         bindWidget();   //Thai is Method
 
+        //button controller
+        buttonController();
+
+
     }// นี่คือ method หลัก
+
+    private void buttonController() {
+
+
+        aboutMEButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //sound
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.dog);
+                buttonMediaPlayer.start();
+
+                //intent to webView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/DBmUc-sKcgg"));
+                startActivity(objIntent);
+            }// event
+        });
+    }
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
