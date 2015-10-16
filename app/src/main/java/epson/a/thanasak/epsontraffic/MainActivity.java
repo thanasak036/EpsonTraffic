03b58b937d1a87eb5457b9df464ddbe7ddd166dc
unknown <ETHAThanasak@ETHATHANASAK.apo.epson.net>
2015-10-16 09:38:59 +0700

    start day2

---
+++ app/src/main/java/epson/a/thanasak/epsontraffic/MainActivity.java
@@ -0,0 +1,31 @@
+package epson.a.thanasak.epsontraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // ประกาศตัวแปร explicit
+    private ListView trafficListView;
+    private Button aboutMEButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //bind widget
+
+        bindWidget();
+
+    }// นี่คือ method หลัก
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMEButton = (Button) findViewById(R.id.button);
+
+
+    }
+}// นี่คือคลาสหลัก main class
